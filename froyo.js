3787a944c9504dd54c8077f1e492bcc1fc67300c
“luciano <lucianoglawson@yahoo.com>
2023-11-15 20:31:39 -0600

    updated code

--- froyo.js
+++ froyo.js
@@ -1,16 +1,29 @@
-/* create prompt for user to input froyo flavors */
-const userInputString = prompt(
-  "Please enter some flavors separated by commas.",
-  "chocolate, vanilla, vanilla, strawberry, oreo, oreo, oreo"
-);
+// Function to count the frequency of froyo flavors
+function countFlavorFrequency(flavorString) {
+  const flavorsArray = flavorString.split(",");
+  const flavorCount = {};
 
-// Split the string of numbers into an array of strings.
-const flavorsArray = userInputString.split(",");
+  flavorsArray.forEach(flavor => {
+    flavorCount[flavor.trim()] = (flavorCount[flavor.trim()] || 0) + 1;
+  });
 
-// Convert the array of strings into an array of numbers.
-const flavorCount = {};
-flavorsArray.forEach(flavor => {
-  ;
-  flavorCount[flavor] = (flavorCount[flavor] || 0) + 1;
-});
-console.log("Flavor Count", flavorCount);
+  return flavorCount;
+}
+
+// Function to prompt the user for froyo flavors and display the results
+function processFroyoOrders() {
+  // Prompt user for froyo flavors
+  const userInputString = prompt(
+    "Please enter some flavors separated by commas.",
+    "chocolate, vanilla, vanilla, strawberry, oreo, oreo, oreo"
+  );
+
+  // Count flavors
+  const flavorCountResult = countFlavorFrequency(userInputString);
+
+  // Display the result in the console
+  console.log("Flavor Count", flavorCountResult);
+}
+
+// Call the function to start the process
+processFroyoOrders();
